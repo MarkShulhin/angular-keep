c0a26a6321d8f7a1dea2129d4073e4b7842afa23
Mark Shulhin <mark.shulhin@gmail.com>
2019-02-11 23:42:49 +0200

    Note edit page

--- src/app/components/note-edit/note-edit.component.ts
+++ src/app/components/note-edit/note-edit.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NoteService } from '../../services/note.service';
+import { Note } from '../../interfaces/note';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-note-edit',
+  selector: 'note-edit',
   templateUrl: './note-edit.component.html',
   styleUrls: ['./note-edit.component.scss']
 })
 export class NoteEditComponent implements OnInit {
+  @Input() note: Note;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private noteService: NoteService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.noteService.getNote(id)
+      .subscribe(note => {this.note = note; console.log(note)});
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  delete(): void {
+    this.noteService.deleteNote(this.note)
+      .subscribe(() => this.goBack());
+  }
+
+  save(): void {
+    this.noteService.updateNote(this.note)
+      .subscribe(() => this.goBack());
+  }
 }
